7119fd80eea505970cd66aade27298a7c5b63dd7
Victor Chern <victor.chern@yahoo.ca>
2016-12-26 21:29:21 -0500

    added missing semicolons

--- merCalc.js
+++ merCalc.js
@@ -8,14 +8,14 @@ var finalAmount = 0;
 initial = 10000;
 finalAmount = initial;
 mer = 0.025;
-dailyMER = mer/365
+var dailyMER = mer/365;
 period = 10;
 annualReturnRate = 0.07;
-dailyReturnRate = annualReturnRate/365
+var dailyReturnRate = annualReturnRate/365;
 
 
 // Calculates the final amount of MER and investment by calculating amount per day
-for(i = 1; i < period*365; i++){
+for(var i = 1; i < period*365; i++){
 	finalAmount = finalAmount+ finalAmount*dailyReturnRate;
 	merTotal = merTotal + finalAmount*dailyMER;
 }
@@ -28,7 +28,7 @@ var finalReturn = Number(Math.round(finalAmount - initial + 'e2')+'e-2');
 
 // Write to document
 $( document ).ready(function() {
-    document.getElementById("results").innerHTML = "The total cost with a MER of " + (mer*100).toFixed(2) + 
+	document.getElementById("results").innerHTML = "The total cost with a MER of " + (mer*100).toFixed(2) + 
 	"% and an initial investment of $" + initial + 
 	" over a period of " + period + " years with an annual rate of return at " + (annualReturnRate*100).toFixed(2) + "% is $"
 	+ merTotal + "!" + " Compare that to the amount that you gained: $" + finalReturn +	"." + " Your final amount is: $" + finalAmount + "."
